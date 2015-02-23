9afc298ed68db5d2657194560c95b3d118f4b9ca
Phil Eichinger <phil@zankapfel.net>
2015-02-23 10:36:56 +0100

    pepperspot: depend on freeradius

--- package/pepperspot/pepperspot.mk
+++ package/pepperspot/pepperspot.mk
@@ -9,5 +9,6 @@ PEPPERSPOT_SOURCE = pepperspot-$(PEPPERSPOT_VERSION).tar.bz2
 PEPPERSPOT_SITE = http://downloads.sourceforge.net/project/pepperspot
 PEPPERSPOT_LICENSE = GPLv2
 PEPPERSPOT_LICENSE_FILES = LICENSE
+PEPPERSPOT_DEPENDENCIES = freeradius
 
 $(eval $(autotools-package))
